79a7f428e7a69f9c8bfcad8cf1fc320488c80c64
happyhyerin333 <gpflswjd0793@gmail.com>
2024-12-14 00:10:10 +0900

    DOCS : Create 20241214.py to practice today what I learned

--- 20241214.py
+++ 20241214.py
@@ -1,1 +1,25 @@
-print('hello')
+## Index
+## My Favorite python Libraries
+
+- fastAPI
+- gunicorn
+- pandas
+- matplotlib
+
+## My Dinner Menu Candidate
+
+1. Pizza
+2. Pasta
+3. Chicken
+4. Beer!
+
+## Snippets
+'''python
+def hello():
+    return 'hello'
+'''
+
+## Links and Images
+[Google](https://www.google.com)
+![](https://www.globalcosmeticsnews.com/wp-content/uploads/2020/04/Google.png)
+
